e09ece31afc6172e4bd7fc8d127300f4248e1883
SaruniaOwO <sara.kleszczewski@gmail.com>
2024-03-13 09:30:18 +0100

    MONGO DB theoretisch eingebunden. TestDaten Datei erstelllt -> tempdata.js

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+
 const app = express();
 const os = require('os');
+
+const clinet = new MongoClient(`mongodb+srv://CatLindaSara:<password>@cluster0.nbomujl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
 
 // Ermittle die IP-Adresse des lokalen Netzwerkinterfaces
 const networkInterfaces = os.networkInterfaces();
 const ipAddress = networkInterfaces['Ethernet'][0].address; // Anpassen, falls du ein anderes Interface verwenden möchtest
 
-app.get('/hello', (req, res) => {
-    res.send('Hello!');
+app.get('/hello', async (req, res) => {
+    await client.connect();
+    const db = client.db('Rezepte-Datenbank')
+    const Rezepte = await db.collection('Rezepte').find({}).toArray();
+    res.send(Rezepte);
 });
 
 app.listen(8000, 'localhost', () => {
---
+++ front-end/src/tempdata.js
@@ -0,0 +1,57 @@
+[
+    {
+      "recipe_name": "Spaghetti Carbonara",
+      "ingredients": [
+        "200g Spaghetti",
+        "100g Pancetta",
+        "2 Eier",
+        "50g Parmesan",
+        "Salz",
+        "Pfeffer"
+      ]
+    },
+    {
+      "recipe_name": "Chicken Curry",
+      "ingredients": [
+        "500g Hähnchenbrust",
+        "1 Zwiebel",
+        "2 Knoblauchzehen",
+        "1 Dose Kokosmilch",
+        "2 EL Currypaste",
+        "1 EL Sojasauce",
+        "1 EL Honig",
+        "Salz",
+        "Pfeffer",
+        "Öl zum Braten"
+      ]
+    },
+    {
+      "recipe_name": "Greek Salad",
+      "ingredients": [
+        "2 Tomaten",
+        "1 Gurke",
+        "1 rote Zwiebel",
+        "100g Feta-Käse",
+        "Olivenöl",
+        "1 EL Rotweinessig",
+        "Salz",
+        "Pfeffer",
+        "Oregano"
+      ]
+    },
+    {
+      "recipe_name": "Chocolate Chip Cookies",
+      "ingredients": [
+        "200g Butter",
+        "200g brauner Zucker",
+        "100g weißer Zucker",
+        "2 Eier",
+        "1 TL Vanilleextrakt",
+        "300g Mehl",
+        "1 TL Backpulver",
+        "200g Schokoladenstückchen",
+        "Salz"
+      ]
+    }
+  ]
+  
